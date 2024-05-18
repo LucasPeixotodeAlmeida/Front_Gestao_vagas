f952e3f495e50ba5dfae58cbcd58306e7ce0cce0
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-05-18 19:15:33 -0300

    Link aplicar backend

--- src/main/java/com/projeto/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/com/projeto/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,5 +1,7 @@
 package com.projeto.front_gestao_vagas.modules.candidate.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,10 +13,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.security.core.Authentication;
 
+import com.projeto.front_gestao_vagas.modules.candidate.service.ApplyJobService;
 import com.projeto.front_gestao_vagas.modules.candidate.service.CandidateService;
 import com.projeto.front_gestao_vagas.modules.candidate.service.FindJobsService;
 import com.projeto.front_gestao_vagas.modules.candidate.service.ProfileCandidateService;
@@ -33,6 +37,9 @@ public class CandidateController {
 
     @Autowired
     private FindJobsService findJobsService;
+
+    @Autowired
+    private ApplyJobService applyJobService;
 
     @GetMapping("/login")
     public String login(){
@@ -92,6 +99,14 @@ public class CandidateController {
         }
         return "/candidate/jobs";
     }
+
+    @PostMapping("/jobs/apply")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public String apllyJob(@RequestParam("jobId") UUID jobId){
+        this.applyJobService.execute(getToken(), jobId);
+        return "redirect:/candidate/jobs";
+    }
+
 
     private String getToken(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
---
+++ src/main/java/com/projeto/front_gestao_vagas/modules/candidate/service/ApplyJobService.java
@@ -0,0 +1,30 @@
+package com.projeto.front_gestao_vagas.modules.candidate.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class ApplyJobService {
+
+    public String execute(String token, UUID idJob){
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<UUID> request = new HttpEntity<>(idJob, headers);
+
+        var result = rt.postForObject("http://localhost:8080/candidate/job/apply", request,  String.class);
+        return result;
+    }
+    
+}
